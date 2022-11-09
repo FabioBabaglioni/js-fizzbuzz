8bc6d46ad3d67e84e55ad0ae973553881ecc59ea
FabioBabaglioni <fabiobabaglioni@gmail.com>
2022-11-09 16:18:28 +0100

    esercizio complete con grafica

--- js/main.js
+++ js/main.js
@@ -6,19 +6,20 @@ for(let i = 1; i <= 100; i++)
 
 // se sono divisibili per due e per 5  allora fizzbuzz
     if(i % 3 === 0 && i % 5 === 0){
-        const element = `<div class = "fizz_buzz square">${i}<!div>`;
+        const element = `<div class = "fizz_buzz square">${"FizzBuzz"}<!div>`;
         contenitore.innerHTML +=element
 
-    }else if(i % 3 ===0){
+    }else if(i % 3 === 0){
         // se sono divisibili per 3 allora fizz
-        const element = `<div class = "fizz square">${i}<!div>`;
+        const element = `<div class = "fizz square">${"Fizz"}<!div>`;
         contenitore.innerHTML +=element
     } else if(i % 5 === 0 ){
         // se sono divisibili per  allora buzz
-        console.log("Buzz")
+        const element = `<div class = "buzz square">${"Buzz"}<!div>`;
+        contenitore.innerHTML +=element
     }else{
         // se no stampa il numero normale
-        const element = `<div class = "buzz square">${i}<!div>`;
+        const element = `<div class = "normal square">${i}<!div>`;
         contenitore.innerHTML +=element
     }
 
