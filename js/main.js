a5d6cfca1eecdb2cb87558414b6dde8575abb87b
FabioBabaglioni <fabiobabaglioni@gmail.com>
2022-11-09 16:03:48 +0100

    clasi colorate css

--- js/main.js
+++ js/main.js
@@ -6,18 +6,20 @@ for(let i = 1; i <= 100; i++)
 
 // se sono divisibili per due e per 5  allora fizzbuzz
     if(i % 3 === 0 && i % 5 === 0){
-        const element = `<div class = "fizz_buzz">${i}<!div>`;
+        const element = `<div class = "fizz_buzz square">${i}<!div>`;
         contenitore.innerHTML +=element
 
     }else if(i % 3 ===0){
         // se sono divisibili per 3 allora fizz
-        console.log("Fizz")
+        const element = `<div class = "fizz square">${i}<!div>`;
+        contenitore.innerHTML +=element
     } else if(i % 5 === 0 ){
         // se sono divisibili per  allora buzz
         console.log("Buzz")
     }else{
         // se no stampa il numero normale
-        console.log(i)
+        const element = `<div class = "buzz square">${i}<!div>`;
+        contenitore.innerHTML +=element
     }
 
     
